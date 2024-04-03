c895bebafc5277ea55b02db214558985359566e6
Sam Grimsley <99296361+SGrimsley02@users.noreply.github.com>
2024-04-03 13:47:51 -0500

    Updated files

--- Lab7/funcs2.js
+++ Lab7/funcs2.js
@@ -6,8 +6,9 @@ function changeColors(){
     var back_r = document.getElementById("back_R").value;
     var back_g = document.getElementById("back_G").value;
     var back_b = document.getElementById("back_B").value;
-
-    document.body.style.borderColor = "rgb(${border_r}, ${border_g}, ${border_b})";
-    document.body.style.backgroundColor = "rgb(${back_r}, ${back_g}, ${back_b})";
-    document.body.style.borderWidth = border_w + 'px';
+    var paragraph = document.getElementById("paragraph");
+    
+    paragraph.style.borderColor = `rgb(${border_r}, ${border_g}, ${border_b})`;
+    document.body.style.backgroundColor = `rgb(${back_r}, ${back_g}, ${back_b})`;
+    paragraph.style.borderWidth = border_w + 'px';
 }
